25fab427f4782a3daa60be1ceeca74b2f213dc12
Jarvan <jarvanzheng@163.com>
2020-11-06 21:38:47 +0800

    创建api文件夹

--- src/views/Yuncun.vue
+++ src/views/Yuncun.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="yuncun">
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      <el-tab-pane label="广场" name="second">5444</el-tab-pane>
+      <el-tab-pane label="关注" name="first">**////</el-tab-pane>
     </el-tabs>
   </div>
 </template>
